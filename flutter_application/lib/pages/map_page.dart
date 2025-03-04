1bfcfcef6d9d62bda967f45b95fc061a8adf6e45
Mia <mia@MiadeMacBook-Air.local>
2025-03-04 23:32:35 +0800

    update 2

--- flutter_application/lib/pages/map_page.dart
+++ flutter_application/lib/pages/map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/pages/home.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -11,17 +12,110 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage>{
 
-  static const LatLng _pGooglePlex = LatLng(35.712479, 139.716924);
+        Location _locationController = new Location();
 
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: 
-          GoogleMap(initialCameraPosition: CameraPosition(
-        target: _pGooglePlex, 
-        zoom:15)),
-          bottomNavigationBar: _bottomBar(context),
-      );
+        late GoogleMapController mapController;
+      
+        final LatLng _center = const LatLng(35.712479, 139.716924);
+      
+        void _onMapCreated(GoogleMapController controller) {
+          mapController = controller;
+        }
+
+        LatLng? _currentP = null;
+
+        @override
+        void initState() {
+          super.initState();
+          getLocationUpdates();
+        }
+
+        @override
+        Widget build(BuildContext context) {
+          return Scaffold(
+            body: Stack( 
+              children: [
+                GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: CameraPosition(
+                    target: _center,
+                    zoom: 15,
+                  ),
+                  markers: {
+                    Marker(
+                      markerId: MarkerId("_currentLocation"),
+                      icon: BitmapDescriptor.defaultMarker,
+                      position: _center,
+                    ),
+                  },
+                ),
+
+                Positioned(
+                  top: 40,
+                  left: 20,
+                  right: 20,
+                  child: _searchBar(), 
+                ),
+              ],
+            ),
+            bottomNavigationBar: _bottomBar(context),
+          );
+        }
+
+  Future<void> getLocationUpdates() async{
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    _serviceEnabled = await _locationController.serviceEnabled();
+    
+    if (_serviceEnabled){
+      _serviceEnabled = await _locationController.requestService();
+    }else{
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if(_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await _locationController.requestPermission();
+      if(_permissionGranted == PermissionStatus.granted){
+        return;
+      }
+    }
+    _locationController.onLocationChanged.listen((LocationData currentLocation){
+      if(currentLocation.latitude != null &&
+         currentLocation.longitude != null){
+           setState(() {
+             _currentP = LatLng(currentLocation.latitude!, currentLocation.longitude!);
+             print(_currentP);
+           });
+         }
+    });
+  }
+}
+
+  Container _searchBar() {
+    return Container(
+          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromARGB(255, 216, 216, 216),
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search for location',
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(
+                  Icons.search,
+                  size: 30,
+                )
+              ),
+
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              )
+            ),
+          ),
+        );
   }
 
   BottomAppBar _bottomBar(BuildContext context) {
@@ -49,4 +143,3 @@ class _MapPageState extends State<MapPage>{
       ),
     );
   }
-}
